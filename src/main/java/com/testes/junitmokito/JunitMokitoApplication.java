3ada1ff2213a084c10a8824187f39fd8bb4cd2f8
DionatanLeao <dionatan.ti@outlook.com>
2022-05-16 12:26:43 -0300

    Project created

---
+++ src/main/java/com/testes/junitmokito/JunitMokitoApplication.java
@@ -0,0 +1,13 @@
+package com.testes.junitmokito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JunitMokitoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JunitMokitoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/testes/junitmokito/JunitMokitoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testes.junitmokito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JunitMokitoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
