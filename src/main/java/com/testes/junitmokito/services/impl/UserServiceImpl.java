daaa422c337663ff646d3a040c37186bd2479c77
DionatanLeao <dionatan.ti@outlook.com>
2022-05-17 14:50:01 -0300

    Endpoint delete

--- src/main/java/com/testes/junitmokito/services/impl/UserServiceImpl.java
+++ src/main/java/com/testes/junitmokito/services/impl/UserServiceImpl.java
@@ -42,8 +42,16 @@ public class UserServiceImpl implements UserService {
 	
 	@Override
 	public User update(UserDTO obj) {
+		findById(obj.getId());
 		findByEmail(obj);
 		return repository.save(mapper.map(obj, User.class));
+	}
+	
+	@Override
+	public void delete(Integer id) {
+		findById(id);
+		repository.deleteById(id);
+		
 	}
 	
 	private void findByEmail(UserDTO obj) {
@@ -52,6 +60,5 @@ public class UserServiceImpl implements UserService {
 			throw new DataIntegratyViolationException("E-mail já cadastrado no sistema");
 		}
 	}
-
 
 }
