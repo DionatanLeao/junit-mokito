094e549cadc198690c8b14d31058b55218d479f9
DionatanLeao <dionatan.ti@outlook.com>
2022-05-16 21:09:56 -0300

    Perfil de teste e Tratamento de exceção para objeto não encontrado

---
+++ src/main/java/com/testes/junitmokito/config/LocalConfig.java
@@ -0,0 +1,29 @@
+package com.testes.junitmokito.config;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.testes.junitmokito.domain.User;
+import com.testes.junitmokito.repositories.UserRepository;
+
+@Configuration
+@Profile("local")
+public class LocalConfig {
+	
+	@Autowired
+	private UserRepository repository;
+	
+	@Bean
+	public void startDB() {
+		User u1 = new User(null, "Mokito 1", "mokito1@email.com", "111111");
+		User u2 = new User(null, "Mokito 2", "mokito2@email.com", "222222");
+		User u3 = new User(null, "Mokito 3", "mokito3@email.com", "333333");
+		
+		repository.saveAll(List.of(u1, u2, u3));
+		
+	}
+}
---
+++ src/main/java/com/testes/junitmokito/services/exception/ObjectNotFoundException.java
@@ -0,0 +1,10 @@
+package com.testes.junitmokito.services.exception;
+
+public class ObjectNotFoundException extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+	
+	public ObjectNotFoundException(String message) {
+		super(message);
+	}
+
+}
