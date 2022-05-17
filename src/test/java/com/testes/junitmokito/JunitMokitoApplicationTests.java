1b03e491fd272f87932d80c4743e296c91c6c450
DionatanLeao <dionatan.ti@outlook.com>
2022-05-17 18:21:22 -0300

    Teste classe principal

--- src/test/java/com/testes/junitmokito/JunitMokitoApplicationTests.java
+++ src/test/java/com/testes/junitmokito/JunitMokitoApplicationTests.java
@@ -7,7 +7,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 class JunitMokitoApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void main() {
+		JunitMokitoApplication.main(new String[] {});
 	}
 
 }
