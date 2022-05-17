ad64d51c87f3d3880d40a2623796134352425817
DionatanLeao <dionatan.ti@outlook.com>
2022-05-17 12:19:37 -0300

    Endpoint findAll

--- src/main/java/com/testes/junitmokito/services/UserService.java
+++ src/main/java/com/testes/junitmokito/services/UserService.java
@@ -1,8 +1,12 @@
 package com.testes.junitmokito.services;
 
+import java.util.List;
+
 import com.testes.junitmokito.domain.User;
 
 public interface UserService {
 	
 	User findById(Integer id);
+	
+	List<User> findAll();
 }
