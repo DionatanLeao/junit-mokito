acc53ccc395dc8573029d164c061595d2637cc79
DionatanLeao <dionatan.ti@outlook.com>
2022-05-19 12:54:03 -0300

    Teste UserResource create

--- src/test/java/com/testes/junitmokito/resources/UserResourceTest.java
+++ src/test/java/com/testes/junitmokito/resources/UserResourceTest.java
@@ -95,7 +95,14 @@ class UserResourceTest {
 
 	@Test
 	void create() {
-
+		when(service.create(Mockito.any())).thenReturn(user);
+		
+		ResponseEntity<UserDTO> response = resource.create(userDTO);
+		
+		assertEquals(ResponseEntity.class, response.getClass());
+		assertEquals(HttpStatus.CREATED, response.getStatusCode());
+		assertNotNull(response.getHeaders().get("Location"));
+		
 	}
 
 	@Test
