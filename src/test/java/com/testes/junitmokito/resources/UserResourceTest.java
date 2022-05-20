96cec668c0c8fd5de26e46e7834880a458da7f53
DionatanLeao <dionatan.ti@outlook.com>
2022-05-20 10:23:06 -0300

    Teste UserResource update

--- src/test/java/com/testes/junitmokito/resources/UserResourceTest.java
+++ src/test/java/com/testes/junitmokito/resources/UserResourceTest.java
@@ -107,7 +107,20 @@ class UserResourceTest {
 
 	@Test
 	void update() {
-
+		when(service.update(userDTO)).thenReturn(user);
+		when(mapper.map(Mockito.any(), Mockito.any())).thenReturn(userDTO);
+		
+		ResponseEntity<UserDTO> response = resource.update(userDTO, ID);
+		
+		assertNotNull(response);
+		assertNotNull(response.getBody());
+		assertEquals(HttpStatus.OK, response.getStatusCode());
+		assertEquals(ResponseEntity.class, response.getClass());
+		assertEquals(UserDTO.class, response.getBody().getClass());
+		
+		assertEquals(ID, response.getBody().getId());
+		assertEquals(NAME, response.getBody().getName());
+		assertEquals(EMAIL, response.getBody().getEmail());
 	}
 
 	@Test
